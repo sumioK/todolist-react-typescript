b9fae64f6a3967078d0005f890f2ceb6db47d59e
Sumio K <tap.miwo@gmail.com>
2022-10-10 20:03:12 +0900

    create shape

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div>
+        <h2>Todoリストwith Typescript</h2>
+        <form onSubmit={() => {}} >
+          <input type="text" onChange={() => {}} className="inputText" />
+          <input type="submit" value="作成" className="submitButton" />
+        </form>
+      </div>
     </div>
   );
 }
